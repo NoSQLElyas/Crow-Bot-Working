81612c5444fd7f7727a3de7948b85b5a7348caf6
NoSQLElyas <154375156+NoSQLElyas@users.noreply.github.com>
2024-10-04 20:55:59 +0200

    Add files via upload

--- commands/bot/theme.js
+++ commands/bot/theme.js
@@ -9,21 +9,18 @@ const {
 
 
 module.exports = {
-	name: 'prefix',
-	aliases: ["setprefix"],
+	name: 'theme',
+	aliases: ["color"],
 	run: async (client, message, args, prefix, color) => {
 
 		if (client.config.owner.includes(message.author.id)) {
 
-
 			let newPrefix = args[0]
 			if (!args[0]) return
 			if (args[1]) return
-			if (db.get(`prefix_${message.guild.id}`) === newPrefix) return message.channel.send(`Le prefix est déjà \`${db.get(`prefix_${message.guild.id}`)}\``)
-			else {
-				db.set(`prefix_${message.guild.id}`, args[0])
-				message.channel.send(`Mon prefix est maintenant : \`${args[0]}\``)
-			}
+			db.set(`color_${message.guild.id}`, args[0])
+			message.channel.send(`Ma couleur d'embed est maintenant : \`${args[0]}\``)
+
 
 		}
 
