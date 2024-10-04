2a85a3b4b5648542bff79128c1ce9b193bb25a0d
NoSQLElyas <154375156+NoSQLElyas@users.noreply.github.com>
2024-10-04 20:59:20 +0200

    Add files via upload

--- commands/utilitaire/invite.js
+++ commands/utilitaire/invite.js
@@ -8,8 +8,8 @@ const {
 } = require('discord-buttons');
 
 module.exports = {
-	name: 'pic',
-	aliases: ["pp"],
+	name: 'invites',
+	aliases: ["invite"],
 	run: async (client, message, args, prefix, color) => {
 		let perm = ""
 		message.member.roles.cache.forEach(role => {
@@ -18,18 +18,22 @@ module.exports = {
 			if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true
 		})
 		if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm || db.get(`channelpublic_${message.guild.id}_${message.channel.id}`) === true) {
+
 			const use = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author
 			const member = client.users.cache.get(use.id)
-			const Embed = new Discord.MessageEmbed()
-			Embed.setTitle(`Cette Photo De Profil Et Incroyable !!!`);
-			Embed.setDescription(`Voici La Photo De Profil De <@${member.id}>`)
-			Embed.setImage(`${member.displayAvatarURL({  dynamic: true })}`);
-			Embed.setTimestamp()
-			Embed.setFooter(`${client.config.name}`)
-			Embed.setColor(color)
-			message.channel.send(Embed)
+			let inv = db.fetch(`invites_${message.guild.id}_${member.id}`);
+			let leaves = db.fetch(`leaves_${message.guild.id}_${member.id}`);
+			let Regular = db.fetch(`Regular_${message.guild.id}_${member.id}`);
+			const embed = new Discord.MessageEmbed()
+			embed.setAuthor('Invitations')
+			embed.setColor(color)
+			embed.setDescription(`<@${member.id}>, Possède Actuellement **${inv || 0}** ${inv || 0 > 1 ? "Invite(s)" : "Invite(s)"}\n(**${Regular || 0}** Join(s), **${leaves || 0}** Leave(s)`)
+			embed.setFooter(`${client.config.name}`)
+			embed.setTimestamp()
+
+			message.channel.send(embed);
+
 
 		}
-
 	}
 }
--- commands/utilitaire/support.js
+++ commands/utilitaire/support.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
+const axios = require("axios");
 const {
 	MessageActionRow,
 	MessageButton,
@@ -8,9 +9,10 @@ const {
 } = require('discord-buttons');
 
 module.exports = {
-	name: 'pic',
-	aliases: ["pp"],
+	name: 'support',
+	aliases: ['aide'],
 	run: async (client, message, args, prefix, color) => {
+
 		let perm = ""
 		message.member.roles.cache.forEach(role => {
 			if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = true
@@ -18,18 +20,17 @@ module.exports = {
 			if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true
 		})
 		if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm || db.get(`channelpublic_${message.guild.id}_${message.channel.id}`) === true) {
-			const use = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author
-			const member = client.users.cache.get(use.id)
-			const Embed = new Discord.MessageEmbed()
-			Embed.setTitle(`Cette Photo De Profil Et Incroyable !!!`);
-			Embed.setDescription(`Voici La Photo De Profil De <@${member.id}>`)
-			Embed.setImage(`${member.displayAvatarURL({  dynamic: true })}`);
-			Embed.setTimestamp()
-			Embed.setFooter(`${client.config.name}`)
-			Embed.setColor(color)
-			message.channel.send(Embed)
 
-		}
+            const embed = new Discord.MessageEmbed()
+            embed.setTitle(`Besoin D'aide ?`)
+            embed.setURL('https://discord.gg/PuqkTuxtFA')
+            embed.setColor(color)
+            embed.setDescription(`Pour rejoindre Le serveur support [Clique ici](https://discord.gg/PuqkTuxtFA)`)
+            embed.setTimestamp()
+            embed.setFooter(`${message.guild.name}`)
 
-	}
+            message.channel.send(embed)
+
+        }
+    }
 }
